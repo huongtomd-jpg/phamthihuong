101291009a0a8c27dc29c7a2e2edcab771c1411b
huongtomd-jpg <huongtomd@gmail.com>
2025-12-12 15:36:33 +0700

    Update app.py

--- app.py
+++ app.py
@@ -347,4 +347,4 @@ if st.button("🚀 SOẠN GIÁO ÁN NGAY"):
 
 # --- CHÂN TRANG ---
 st.markdown("---")
-st.markdown("<div style='text-align: center; color: #666;'>© 2025 - phạm Thị Hương - Trường Tiểu học Minh Dân - ĐT: 0941 273 417", unsafe_allow_html=True)
+st.markdown("<div style='text-align: center; color: #666;'>© 2025 - phạm Thị Hương - Trường Tiểu học Minh Dân - ĐT: 0941 273 417</p> unsafe_allow_html=True)
